291582a9a3b4b74be364f17fa0e8c73f1522e573
Lacccka <101393701+Lacccka@users.noreply.github.com>
2026-06-03 23:14:23 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,16 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-}
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
 
 dependencies {
     testImplementation("junit:junit:4.13.2")
